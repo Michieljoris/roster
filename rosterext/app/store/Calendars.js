9f8bbe03d98ca2d972082eb1465aefdfe07b71d0
Michiel van Oosten <michieljoris@gmail.com>
2013-01-19 13:06:51 +1000

    check

--- rosterext/app/store/Calendars.js
+++
@@ -1,9 +0,0 @@
-Ext.define(
-    'AM.store.Calendars', {
-	extend: 'Extensible.calendar.data.MemoryCalendarStore'
-	// ,model:'AM.model.Calendar'
-	//,autoload: true
-	,data: Ext.create('data.example.Calendars')
-    }
-);
-
