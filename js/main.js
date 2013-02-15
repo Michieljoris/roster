9f8ce776975b5e64ab2c4d98aca6adaf22f226cc
michieljoris <michieljoris@gmail.com>
2013-02-16 01:53:09 +1000

    Progress bar while loading the cache

--- js/main.js
+++ js/main.js
@@ -26,60 +26,6 @@ define(
           //Are we alive? 
           log.d('Starting up app...');
           
-          var appCache = window.applicationCache;
-          
-          function handleCacheEvent(e) {
-              // log.d('my appcache event:', e);
-              if (e.type === 'updateready')
-                  
-                  if (appCache.status === appCache.UPDATEREADY) {
-                      // Browser downloaded a new app cache.
-                      // Swap it in and reload the page to get the new hotness.
-                      window.applicationCache.swapCache();
-                      if (confirm('A new version of this web application is available. If you select Ok, the browser will refresh the page and load the new version. If you select Cancel you can continue using the app as it is, and the browser will load the new version next time the page is refreshed (by F5, Control-r or a restart of the browser).')) {
-                          window.location.reload();
-                      }
-                  } else {
-                      // Manifest didn't changed. Nothing new to server.
-                  }
-              //...
-          }
-
-          function handleCacheError(e) {
-              alert('Error: Cache failed to update!');
-              log.e(e);
-          }
-
-          // Fired after the first cache of the manifest.
-          appCache.addEventListener('cached', handleCacheEvent, false);
-
-          // Checking for an update. Always the first event fired in the sequence.
-          appCache.addEventListener('checking', handleCacheEvent, false);
-
-          // An update was found. The browser is fetching resources.
-          appCache.addEventListener('downloading', handleCacheEvent, false);
-
-          // The manifest returns 404 or 410, the download failed,
-          // or the manifest changed while the download was in progress.
-          appCache.addEventListener('error', handleCacheError, false);
-
-          // Fired after the first download of the manifest.
-          appCache.addEventListener('noupdate', handleCacheEvent, false);
-
-          // Fired if the manifest file returns a 404 or 410.
-          // This results in the application cache being deleted.
-          appCache.addEventListener('obsolete', handleCacheEvent, false);
-
-          // Fired for each resource listed in the manifest as it is being fetched.
-          appCache.addEventListener('progress', handleCacheEvent, false);
-
-          // Fired when the manifest resources have been newly redownloaded.
-          appCache.addEventListener('updateready', handleCacheEvent, false);
-          
-          // applicationCache.addeventListener(applicationCache.UPDATEREADY, function() {
-          //     alert('Update ready');
-              
-          // });
       
           //##Startup logic.  We try to get the cookie with info on
           //the database last used. If present we set up the app with
@@ -200,6 +146,7 @@ define(
                       // layout.draw();
                       try { layout.draw();
                             document.getElementById('appFail').style.display = 'none';
+                            document.getElementById('statusUpdate').style.display = 'none';
                           } catch(e) {
                           console.error(e.stack); }
                       
