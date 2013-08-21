8e47386edc744fd9bbaf37fcd4906c7b6ad254f2
Michiel van Oosten <michieljoris@gmail.com>
2013-08-21 21:17:49 +1000

    test ungit

--- couchdb_js/validator.js
+++ couchdb_js/validator.js
@@ -16,6 +16,49 @@ function isArray(value) {
 function isDate(value){
     return Object.prototype.toString.apply(value) === '[object Date]';
 }
+bla
+
+function equals(o1, o2) {
+    
+    if (o1 === o2) return true;
+    if (o1 === null || o2 === null) return false;
+    if (o1 !== o1 && o2 !== o2) return true; // NaN === NaN
+    var t1 = typeof o1, t2 = typeof o2, length, key, keySet;
+        if (t1 === t2) {
+            if (t1 === 'function') {
+                return t1.toString() === t2.toString();
+            }
+            else if (t1 === 'object') {
+                if (isArray(o1)) {
+                    if (!isArray(o2)) return false;
+                    if ((length = o1.length) === o2.length) {
+                        for(key=0; key<length; key++) {
+                            if (!equals(o1[key], o2[key])) return false;
+                        }
+                        return true;
+                        
+                    }
+                } else if (isDate(o1)) {
+                    return isDate(o2) && o1.getTime() === o2.getTime();
+                } else {
+                    if (isArray(o2)) return false;
+                    keySet = {};
+                    for(key in o1) {
+                        if (!equals(o1[key], o2[key])) return false;
+                        keySet[key] = true;
+                    }
+                    for(key in o2) {
+                        if (!keySet.hasOwnProperty(key) &&
+                            o2[key] !== undefined ) return false;
+                    }
+                    return true;
+                }
+            }
+        }
+    return false;
+}
+
+
 
 var code = {
     defined: {
@@ -73,45 +116,6 @@ var test = (function() {
     return obj;
 })();
 
-
-function equals(o1, o2) {
-    
-    if (o1 === o2) return true;
-    if (o1 === null || o2 === null) return false;
-    if (o1 !== o1 && o2 !== o2) return true; // NaN === NaN
-    var t1 = typeof o1, t2 = typeof o2, length, key, keySet;
-        if (t1 === t2) {
-            if (t1 === 'function') {
-                return t1.toString() === t2.toString();
-            }
-            else if (t1 === 'object') {
-                if (isArray(o1)) {
-                    if (!isArray(o2)) return false;
-                    if ((length = o1.length) === o2.length) {
-                        for(key=0; key<length; key++) {
-                            if (!equals(o1[key], o2[key])) return false;
-                        }
-                        return true;
-                    }
-                } else if (isDate(o1)) {
-                    return isDate(o2) && o1.getTime() === o2.getTime();
-                } else {
-                    if (isArray(o2)) return false;
-                    keySet = {};
-                    for(key in o1) {
-                        if (!equals(o1[key], o2[key])) return false;
-                        keySet[key] = true;
-                    }
-                    for(key in o2) {
-                        if (!keySet.hasOwnProperty(key) &&
-                            o2[key] !== undefined ) return false;
-                    }
-                    return true;
-                }
-            }
-        }
-    return false;
-}
 
 function parseDbRule(rule) {
     rule = 'rule = {' + rule + '}';
